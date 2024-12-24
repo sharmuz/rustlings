652a647c91ee3f3c316ca7e123b57e79ca2af8e2
sharmuz <muzher@gmail.com>
2024-12-24 11:04:14 +0000

    Adjust solution to lifetimes exercises

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -19,7 +19,7 @@ fn main() {
     let result;
     {
         let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
+        result = longest(&string1, &string2);
         println!("The longest string is '{}'", result);
     }
     // println!("The longest string is '{}'", result);
